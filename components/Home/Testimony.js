b8eaa571a37ee89585c0773c20b7b6a792f82771
SteffQing <steveola23@gmail.com>
2023-03-29 11:16:08 +0100

    First iteration of the testimony box scroll

--- components/Home/Testimony.js
+++ components/Home/Testimony.js
@@ -6,7 +6,27 @@ import Ghana from "../../assets/Ghana";
 import Naija from "../../assets/Naija";
 import Quotes from "../../assets/Quotes";
 import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import useMQ from "../../hooks/useMQ";
 export default function Testimony() {
+  const isMobile = useMQ("(max-width: 400px)");
+  const isTablet = useMQ("(max-width: 900px)");
+  const isPC = useMQ("(max-width: 1200px)");
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: isMobile ? 1 : isTablet ? 2 : isPC ? 3 : 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    nextArrow: <SampleArrow />,
+    prevArrow: <SampleArrow />,
+    pauseOnHover: true,
+  };
   return (
     <div className={styles.Testimony}>
       <section className={styles.Blobs}>
@@ -25,7 +45,7 @@ export default function Testimony() {
             textAlign: "center",
           }}
         />
-        <div className={styles.Testimonies}>
+        <Slider {...settings} className={styles.Testimonies}>
           {Testimonies.map((testimony, i) => (
             <aside key={i} className={styles.TestimonyBox}>
               <div className={styles.Top}>
@@ -34,12 +54,14 @@ export default function Testimony() {
               </div>
               <Paragraphs content={testimony.text} />
               <div className={styles.Bottom}>
-                <Image
-                  src={testimony.photo}
-                  width="200px"
-                  height="200px"
-                  style={{ borderRadius: "50%" }}
-                />
+                <div className={styles.BottomImage}>
+                  <Image
+                    src={testimony.photo}
+                    objectFit="contain"
+                    layout="fill"
+                    style={{ borderRadius: "50%" }}
+                  />
+                </div>
                 <div className={styles.BottomInfo}>
                   <b>{testimony.name}</b>
                   <span>{testimony.more}</span>
@@ -47,12 +69,26 @@ export default function Testimony() {
               </div>
             </aside>
           ))}
-        </div>
+        </Slider>
       </section>
     </div>
   );
 }
 let Testimonies = [
+  {
+    country: <Ghana />,
+    text: "I appreciate the customer supports they were very responsive and resolved them in time, Thank you Educart",
+    photo: TestimonyImage,
+    name: "Esene Godwin",
+    more: "MBA. Industrial Design, Imperial College London, Accra Ghana",
+  },
+  {
+    country: <Ghana />,
+    text: "I appreciate the customer supports they were very responsive and resolved them in time, Thank you Educart",
+    photo: TestimonyImage,
+    name: "Esene Godwin",
+    more: "MBA. Industrial Design, Imperial College London, Accra Ghana",
+  },
   {
     country: <Ghana />,
     text: "I appreciate the customer supports they were very responsive and resolved them in time, Thank you Educart",
@@ -75,3 +111,13 @@ let Testimonies = [
     more: "MBA. Industrial Design, Imperial College London, Accra Ghana",
   },
 ];
+function SampleArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "none", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
