ce3cc23da7aaf9e025a101243cb58073e2019abf
SteffQing <steveola23@gmail.com>
2023-04-21 10:21:35 +0100

    Completed Payments Page and Components

---
+++ assets/PersonalInfo.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+const Person = (props) => (
+  <svg
+    width={126}
+    height={126}
+    viewBox="0 0 126 126"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M63 62.9991C77.4075 62.9991 89.087 51.3196 89.087 36.9122C89.087 22.5047 77.4075 10.8252 63 10.8252C48.5926 10.8252 36.9131 22.5047 36.9131 36.9122C36.9131 51.3196 48.5926 62.9991 63 62.9991Z"
+      stroke="#20466F"
+      strokeWidth={5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M18.1826 115.173C18.1826 94.9818 38.2696 78.6514 63 78.6514C68.0087 78.6514 72.8609 79.3296 77.4 80.5818"
+      stroke="#20466F"
+      strokeWidth={5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M115.175 94.3041C115.175 95.9737 114.966 97.5911 114.549 99.1563C114.079 101.243 113.244 103.278 112.149 105.052C108.549 111.104 101.923 115.174 94.3051 115.174C88.9312 115.174 84.079 113.139 80.4268 109.8C78.8616 108.443 77.505 106.826 76.4616 105.052C74.5311 101.922 73.4355 98.2172 73.4355 94.3041C73.4355 88.6694 75.6791 83.5042 79.3312 79.7477C83.1399 75.8347 88.4616 73.4346 94.3051 73.4346C100.462 73.4346 106.044 76.0955 109.801 80.3738C113.14 84.0781 115.175 88.9824 115.175 94.3041Z"
+      stroke="#20466F"
+      strokeWidth={5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M102.079 94.1992H86.5312"
+      stroke="#20466F"
+      strokeWidth={5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M94.3047 86.582V102.182"
+      stroke="#20466F"
+      strokeWidth={5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default Person;
--- pages/payments/sevis.js
+++ pages/payments/sevis.js
@@ -12,20 +12,40 @@ import Button from "../../components/General/Button";
 import { useEffect, useState } from "react";
 import RadioGroup from "../../components/General/Radio";
 import { GrValidate } from "react-icons/gr";
+import { RxCopy } from "react-icons/rx";
 import Input from "../../components/General/Input";
 import Nigeria from "../../assets/Nigeria";
 import Idea from "../../assets/Idea";
 import Document from "../../assets/Document";
+import Cloud from "../../assets/Cloud";
+import Person from "../../assets/PersonalInfo";
+import MasterCard from "../../assets/MasterCard";
+import Visa from "../../assets/Visa";
+import Paypal from "../../assets/Paypal";
+import Lock from "../../assets/Lock";
+import InfoModal from "../../assets/InfoModal";
+import Timer from "../../assets/Timer";
+import GTBankLogo from "../../assets/GTBank";
+let gloablButtonStyle = {
+  height: "auto",
+  margin: "0 auto 2rem",
+  padding: "10px 50px",
+  width: "min(100%, 300px)",
+};
 export default function SevisPage() {
   const [state, setState] = useState(0);
+  let isMobile = useMQ("(max-width: 700px)");
   return (
     <main className={styles.SevisPage}>
       <ChatSupportBox />
       {(state === 0 || state === 1) && <HeaderElement />}
       {state === 0 && <OptionsElement setState={setState} />}
       {state === 1 && <StartSevisPayment setState={setState} />}
-      {state === 2 && <SevisHeader />}
-      {state === 2 && <SevisValidation />}
+      {state === 2 || (state === 3 && <SevisHeader />)}
+      {state === 2 && (
+        <SevisValidation isMobile={isMobile} setState={setState} />
+      )}
+      {state === 3 && <PaymentMethod isMobile={isMobile} setState={setState} />}
     </main>
   );
 }
@@ -103,7 +123,7 @@ function StartSevisPayment({ setState }) {
   };
   useEffect(() => {
     if (selectedOption === "auto") setState(2);
-    if (selectedOption === "user") setState(4);
+    if (selectedOption === "user") setState(3);
   }, [selectedOption]);
   let radioOptions = [
     { value: "auto", label: "Carry out all the SEVIS Fee Payment for me" },
@@ -138,12 +158,9 @@ function SevisHeader() {
   );
 }
 
-function SevisValidation({ setState }) {
-  let position = [
-    "Applicants Information",
-    "Payment Method",
-    "Confirm Payment",
-  ];
+let position = ["Applicants Information", "Payment Method", "Confirm Payment"];
+
+function SevisValidation({ setState, isMobile }) {
   const [selectedOption, setSelectedOption] = useState("DS 2019 J-1");
   const [localState, setLocalState] = useState(false);
   const handleOptionChange = (value) => {
@@ -169,13 +186,12 @@ function SevisValidation({ setState }) {
       ),
     },
   ];
-  let isMobile = useMQ("(max-width: 700px)");
   return (
     <section className={styles.SevisValidation}>
       {isMobile ? (
-        <MobilePointer position={position} />
+        <MobilePointer position={position} index={0} />
       ) : (
-        <Pointer position={position} />
+        <Pointer position={position} index={0} />
       )}
       <section className={styles.Sevis}>
         {localState ? (
@@ -183,25 +199,70 @@ function SevisValidation({ setState }) {
         ) : (
           <Form1
             selectedOption={selectedOption}
-            setLocalState={setLocalState}
             isMobile={isMobile}
             radioOptions={radioOptions}
             handleOptionChange={handleOptionChange}
           />
         )}
-        {isMobile ? null : <Aside />}
+        {isMobile ? null : <Aside state={localState} />}
       </section>
+      <Button
+        action={localState ? () => setState(3) : () => setLocalState(true)}
+        content={localState ? "Submit" : "Next"}
+        style={gloablButtonStyle}
+      />
     </section>
   );
 }
 
-function Form1({
-  setLocalState,
-  isMobile,
-  radioOptions,
-  selectedOption,
-  handleOptionChange,
-}) {
+function PaymentMethod({ setState, isMobile }) {
+  const [selectedOption, setSelectedOption] = useState("card");
+  const [localState, setLocalState] = useState(false);
+  const handleOptionChange = (value) => {
+    setSelectedOption(value);
+  };
+  let radioOptions = [
+    { value: "card", label: "Credit/Debit Card" },
+    { value: "bank", label: "Bank Transfer" },
+  ];
+  return (
+    <section className={styles.PaymentMethod}>
+      {" "}
+      {isMobile ? (
+        <MobilePointer position={position} index={1} />
+      ) : (
+        <Pointer position={position} index={1} />
+      )}
+      <div className={styles.paymentId}>
+        <Span content="Payment ID - EDU2349856" style={{ color: "#7E7E80" }} />
+        <i>
+          Copy <RxCopy />
+        </i>
+      </div>
+      <aside className={styles.paymentBox}>
+        {!localState && (
+          <RadioGroup
+            options={radioOptions}
+            selectedOption={selectedOption}
+            onChange={handleOptionChange}
+          />
+        )}
+        {selectedOption === "card" ? (
+          localState ? (
+            <CompleteCardPayment isMobile={isMobile} />
+          ) : (
+            <CardPayment isMobile={isMobile} setLocalState={setLocalState} />
+          )
+        ) : (
+          <BankPayment isMobile={isMobile} />
+        )}
+      </aside>
+    </section>
+  );
+}
+
+// Start of Sevis Validation Components
+function Form1({ isMobile, radioOptions, selectedOption, handleOptionChange }) {
   return (
     <aside className={styles.Info}>
       <aside className={styles.ValidationInfo}>
@@ -277,20 +338,10 @@ function Form1({
           />
         </div>
       </aside>
-      <Button
-        action={() => setLocalState(true)}
-        content="Next"
-        style={{
-          height: "auto",
-          margin: "0 auto 2rem",
-          padding: "10px 50px",
-          width: isMobile ? "300px" : "",
-        }}
-      />
     </aside>
   );
 }
-function Form2({ setState, isMobile }) {
+function Form2({ isMobile }) {
   return (
     <aside className={styles.Info}>
       <aside className={styles.ValidationInfo}>
@@ -326,36 +377,46 @@ function Form2({ setState, isMobile }) {
             style={{ color: "#696A6A", fontSize: "22px" }}
           />
           <Input
-            placeholder="Select a file or drag and Drop .Jpg or .Png"
+            placeholder={
+              <>
+                <Cloud />
+                <span>
+                  <i>Select a file</i> or drag and Drop .Jpg or .Png
+                </span>
+              </>
+            }
             label="IS-19/DS 20"
             type="file"
           />
           <Input
-            placeholder="Select a file or drag and Drop .Jpg or .Png"
+            placeholder={
+              <>
+                <Cloud />
+                <span>
+                  <i>Select a file</i> or drag and Drop .Jpg or .Png
+                </span>
+              </>
+            }
             label="Passport Photograph"
             type="file"
           />
           <Input
+            placeholder={
+              <>
+                <Cloud />
+                <span>
+                  <i>Select a file</i> or drag and Drop .Jpg or .Png
+                </span>
+              </>
+            }
             label="International Passport Data Page"
             type="file"
-            placeholder="Select a file or drag and Drop .Jpg or .Png"
           />
         </div>
       </aside>
-      <Button
-        action={() => setState(3)}
-        content="Submit"
-        style={{
-          height: "auto",
-          margin: "0 auto 2rem",
-          padding: "10px 50px",
-          width: isMobile ? "300px" : "",
-        }}
-      />
     </aside>
   );
 }
-
 function Aside({ state }) {
   return (
     <div className={styles.Aside}>
@@ -364,7 +425,7 @@ function Aside({ state }) {
         <div className={styles.doc1}>
           <div className={styles.doc2}>
             <div className={styles.doc3}>
-              <Document />
+              {state ? <Document /> : <Person />}
             </div>
           </div>
         </div>
@@ -372,7 +433,6 @@ function Aside({ state }) {
     </div>
   );
 }
-
 function Note() {
   return (
     <div className={styles.Note}>
@@ -385,8 +445,177 @@ function Note() {
     </div>
   );
 }
+// End of Sevis Validation Components
 
-function Pointer({ position }) {
+// Start of PaymentMethod components
+function CardPayment({ isMobile, setLocalState }) {
+  return (
+    <div className={styles.CardPayment}>
+      <div className={styles.Svg}>
+        <MasterCard />
+        <Visa />
+        <Paypal />
+      </div>
+      <div
+        className={styles.Note}
+        style={{ padding: isMobile ? "12px" : "20px" }}
+      >
+        <div className={styles.top} style={{ alignItems: "flex-start" }}>
+          <div className={styles.lock}>
+            <Lock />
+          </div>
+          <Paragraphs
+            content="We do not store any credit card Information and your payments are processed by world leading payment gateways"
+            style={{ maxWidth: isMobile ? "90%" : "70%" }}
+          />
+        </div>
+      </div>
+      <div className={styles.cardForm}>
+        <Input
+          label="Card Name"
+          placeholder="Enter Card Holders Name"
+          labelAfter="*"
+        />
+        <Input
+          label="Card Number"
+          placeholder="0000 0000 0000"
+          labelAfter="*"
+        />
+        <div className={styles.sensitiveCardInfo}>
+          <div className={styles.expiry}>
+            <Input label="Expiry Date" placeholder="Month" labelAfter="*" />
+            <Input placeholder="Year" label="." />
+          </div>
+          <Input label="CVV" placeholder="123" labelAfter="*" />
+        </div>
+      </div>
+      <div className={styles.InfoModal}>
+        <InfoModal />
+        Credit Card payments maight take up to 24 hours to be processed
+      </div>
+      <Button
+        content="Next"
+        action={() => setLocalState(true)}
+        style={gloablButtonStyle}
+      />
+    </div>
+  );
+}
+function CompleteCardPayment({ isMobile }) {
+  let PaymentSummary = [
+    { header: "Paying For", content: "SEVIS FEE(1-90)" },
+    { header: "Form Fee", content: "$450.00" },
+    { header: "Charge", content: "$120.00" },
+    { header: "Total", content: "$670.00" },
+  ];
+  return (
+    <div className={styles.CompleteCardPayment}>
+      <PaymentsNote
+        content="Kindly Double Check The Details Below Before You Click Pay"
+        isMobile={isMobile}
+      />
+      <div className={styles.PaymentDetails}>
+        <div className={styles.top}>
+          <Span
+            content={
+              <>
+                Payment Details
+                <GrValidate />
+              </>
+            }
+          />
+          <i>Edit</i>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.cardDetails}>
+            <MasterCard />
+            <aside>
+              {[1, 2, 3].map((Dots) => (
+                <span className={styles.Dots} key={Dots}>
+                  {[1, 2, 3, 4].map((Dot) => (
+                    <i className={styles.dot} key={Dot}></i>
+                  ))}
+                </span>
+              ))}
+              <span className={styles.lastNum}>3584</span>
+            </aside>
+          </div>
+          <Span content="Master Card" style={{ fontSize: "14px" }} />
+        </div>
+      </div>
+      <div className={styles.PaymentSummary}>
+        {" "}
+        <div className={styles.top}>
+          <Span content="Payment Summary" />
+          <i>Edit</i>
+        </div>
+        <div className={styles.Details}>
+          {PaymentSummary.map(({ header, content }) => (
+            <div className={styles.Detail}>
+              <Span content={header} style={{ color: "#A6A6A8" }} />{" "}
+              <span>|</span> <Span content={content} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <Button content="Pay" style={gloablButtonStyle} />
+    </div>
+  );
+}
+function BankPayment({ isMobile }) {
+  let Details = [
+    { header: "Account Name", detail: "Educarts LLC" },
+    { header: "Account Number", detail: "1234567890" },
+    { header: "Bank Name", detail: "GTCO" },
+    { header: "Account Type", detail: "Current" },
+  ];
+  return (
+    <div className={styles.BankPayment}>
+      <PaymentsNote
+        content="Bank Transfer can take up to 15 minutes to be confirmed, Please check Your Payment Status to confirm your payment status"
+        isMobile={isMobile}
+      />
+      <div className={styles.accountBox}>
+        <div className={styles.box}>
+          <Span content="Make a transfer to the account details below" />
+          <div className={styles.Bank}>
+            <GTBankLogo />
+            <HighlightHeader content="Educarts LLC" />
+          </div>
+          <div className={styles.Details}>
+            {Details.map(({ header, detail }) => (
+              <div className={styles.Detail}>
+                <Span content={header} style={{ color: "#A6A6A8" }} />{" "}
+                <span>|</span> <Span content={detail} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Button content="I Have Made Payments" style={gloablButtonStyle} />
+    </div>
+  );
+}
+function PaymentsNote({ content, isMobile }) {
+  return (
+    <div
+      className={styles.Note}
+      style={{ padding: isMobile ? "12px" : "20px", width: "min(400px, 100%)" }}
+    >
+      <div className={styles.top}>
+        <div className={styles.lock}>
+          <Timer />
+        </div>
+        <Paragraphs
+          content={content}
+          style={{ maxWidth: isMobile ? "90%" : "70%" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Pointer({ position, index }) {
   let CircleStyle = { background: "#696A6A" };
   let scale = { scale: 0.7 };
   return (
@@ -396,11 +625,11 @@ function Pointer({ position }) {
           <div className={styles.MainPointer}>
             <div
               className={styles.OuterCircle}
-              style={i != 0 ? { ...scale, ...CircleStyle } : {}}
+              style={index != i ? { ...scale, ...CircleStyle } : {}}
             >
               <div
                 className={styles.InnerCircle}
-                style={i != 0 ? CircleStyle : {}}
+                style={index != i ? CircleStyle : {}}
               >
                 <span>{i + 1}</span>
               </div>
@@ -415,18 +644,17 @@ function Pointer({ position }) {
     </div>
   );
 }
-
-function MobilePointer({ position }) {
+function MobilePointer({ position, index }) {
   return (
     <div className={styles.MobilePointer}>
       <div className={styles.Point}>
         <div className={styles.MainPointer}>
           <div className={styles.OuterCircle}>
             <div className={styles.InnerCircle}>
-              <span>{1}</span>
+              <span>{index + 1}</span>
             </div>
           </div>
-          <Paragraphs content={position[0]} />
+          <Paragraphs content={position[index]} />
         </div>
       </div>
     </div>
